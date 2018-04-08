c5970a4b1bbd285ea23b9f90d85c0f3222fbf7c4
wyy <wyy@zntxkj.com>
2018-04-08 18:07:11 +0800

    ConstraintLayout 学习

--- app/src/main/java/com/sunny/beauty/personalcenter/FirstFragment.java
+++ app/src/main/java/com/sunny/beauty/personalcenter/FirstFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import com.sunny.beauty.R;
 import com.sunny.beauty.ViewPagerActivity;
 import com.sunny.beauty.rv.CaptureActivity;
+import com.sunny.beauty.view.ConstraintLayoutActivity;
 import com.sunny.beauty.view.CustomViewActivity;
 
 import butterknife.ButterKnife;
@@ -38,6 +39,8 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
     TextView item2Tv;
     @InjectView(R.id.item3_tv)
     TextView item3Tv;
+    @InjectView(R.id.item4_tv)
+    TextView item4Tv;
 
     private String newStr = "CenterFragment";
 
@@ -54,6 +57,7 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
         item1Tv.setOnClickListener(this);
         item2Tv.setOnClickListener(this);
         item3Tv.setOnClickListener(this);
+        item4Tv.setOnClickListener(this);
     }
 
     public void setText(String page) {
@@ -77,6 +81,9 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.item3_tv:
                 startDrawViewActivity();
+                break;
+            case R.id.item4_tv:
+                startConstraintLayoutActivity();
                 break;
             default:
                 break;
@@ -140,6 +147,19 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
     private void startDrawViewActivity() {
         try {
             Intent intent = new Intent(getActivity(), CustomViewActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 启动 ConstraintLayout 测试布局类
+     */
+    private void startConstraintLayoutActivity() {
+        try {
+            Intent intent = new Intent(getActivity(), ConstraintLayoutActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
         } catch (ActivityNotFoundException e) {
---
+++ app/src/main/java/com/sunny/beauty/view/ConstraintLayoutActivity.java
@@ -0,0 +1,22 @@
+package com.sunny.beauty.view;
+
+import android.os.Bundle;
+
+import com.sunny.beauty.R;
+
+import sunny.basemodel.BaseActivity;
+
+/**
+ * author : wyy
+ * time   : 2018/02/24
+ * desc   :ConstraintLayout 测试布局类
+ * 参考链接：https://blog.csdn.net/lmj623565791/article/details/78011599?utm_source=tuicool&utm_medium=referral
+ */
+
+public class ConstraintLayoutActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout_test);
+    }
+}
